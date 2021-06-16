06aa812a5c646f11304f8d2c41c1fe86aba38788
Shuvam Sengupta <shuvamsengupta58824@gmail.com>
2021-06-16 05:01:47 -0700

    Django Settings Done

---
+++ landing/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path,include
+from .views import *
+
+urlpatterns = [
+
+path('', home , name="home"),
+path('Services/', services , name="services"),
+path('Contact_us/', contact_us , name="contact_us"),
+path('Services/Lean_Manufacturing', lean_manufacturing , name="lean_manufacturing"),
+path('Services/Smart_Factory_IoT', smart_factory , name="smart_factory"),
+path('Services/TPM', tpm , name="tpm"),
+path('Services/Innovation_Management', innovation_management , name="innovation_management"),
+path('Services/Service_and_Sales', services_and_sales , name="services_and_sales"),
+path('Services/Organization_and_HR', organization_and_hr , name="organization_and_hr"),
+path('Training/Training_for_Manufacture', training_for_manufacture , name="training_for_manufacture"),
+path('Training/P-Course', p_course , name="p_course"),
+path('Training/Kart_Factory', kart_factory , name="kart_factory"),
+
+
+
+
+
+]
--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	return render(request,'home.html')
+
+def services(request):
+	return HttpResponse('Services Page')
+
+def contact_us(request):
+	return HttpResponse('Contact Us Page')
+
+def lean_manufacturing(request):
+	return HttpResponse('Lean Manufacturing Page')
+
+def smart_factory(request):
+	return HttpResponse('Home Page')
+
+def tpm(request):
+	return HttpResponse('Home Page')
+
+
+def innovation_management(request):
+	return HttpResponse('Home Page')
+
+
+def services_and_sales(request):
+	return HttpResponse('Home Page')
+
+
+def organization_and_hr(request):
+	return HttpResponse('Home Page')
+
+def training_for_manufacture(request):
+	pass
+
+def p_course(request):
+	pass
+
+def kart_factory(request):
+	pass
