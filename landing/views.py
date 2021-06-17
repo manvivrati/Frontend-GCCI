48f2353bbf2b16b7e83a5363b9fd3320f66d8bbd
manvivrati <manvivrati@gmail.com>
2021-06-17 19:30:08 +0530

    NEWS WIP

--- landing/views.py
+++ landing/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def home(request):
 	return render(request, 'home.html')
+
+
+def news(request):
+	return render(request, 'news.html')
+
 
 def services(request):
 	return render(request, 'services.html')
 
+
 def contact_us(request):
 	return HttpResponse('Contact Us')
+
 
 def lean_manufacturing(request):
 	return render(request, 'Lean_Manufacture.html')
 
+
 def smart_factory(request):
 	return render(request, 'smartfactory.html')
+
 
 def tpm(request):
 	return HttpResponse('Home Page')
@@ -32,11 +41,14 @@ def services_and_sales(request):
 def organization_and_hr(request):
 	return render(request, 'organization_hr.html')
 
+
 def training_for_manufacture(request):
 	return render(request, 'manufacture_page.html')
 
+
 def p_course(request):
 	return render(request, 'pcourse.html')
+
 
 def kart_factory(request):
 	return render(request, 'kartfactory.html')
