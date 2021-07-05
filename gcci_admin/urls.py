b53c9f57a25ed3febb8817352437fb415df11f55
Shuvam Sengupta <shuvamsengupta58824@gmail.com>
2021-07-05 13:54:53 -0700

    Admin Dashboard

--- gcci_admin/urls.py
+++ gcci_admin/urls.py
@@ -4,5 +4,12 @@ from .views import *
 urlpatterns = [
 path("", admin_login, name="admin_login"),
 path("Dashboard/", admin_homepage, name="admin_homepage"),
+path("Dashboard/Add_Events", add_events, name="add_events"),
+path("Dashboard/Manage_Events", manage_events, name="manage_events"),
+path("Dashboard/Add_News", add_news, name="add_news"),
+path("Dashboard/Manage_News", manage_news, name="manage_news"),
+path("Dashboard/Add_Blogs", add_blogs, name="add_blogs"),
+path("Dashboard/Manage_Blogs", manage_blogs, name="manage_blogs"),
+path("Dashboard/Summary", summary, name="summary"),
 
 ]
--- gcci_admin/views.py
+++ gcci_admin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+# Create your views here. 
 
 
 def admin_homepage(request):
@@ -9,3 +9,26 @@ def admin_homepage(request):
 
 def admin_login(request):
 	return render(request, 'admin_login.html')
+
+def add_events(request):
+	return render(request, 'add_events.html')
+
+def manage_events(request):
+	return render(request, 'manage_events.html')
+
+def add_news(request):
+	return render(request, 'add_news.html')
+
+def manage_news(request):
+	return render(request, 'manage_news.html')
+
+
+def add_blogs(request):
+	return render(request, 'add_blog.html')
+
+def manage_blogs(request):
+	return render(request, 'manage_blog.html')
+
+
+def summary(request):
+	return render(request,'dashboard.html')
